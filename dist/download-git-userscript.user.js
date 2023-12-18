2e40a63081b7f0cc243d292aee26fc2d45a52153
Saiya <oklark@outlook.com>
2023-12-18 15:02:32 +0800

    chore: update version

--- dist/download-git-userscript.user.js
+++ dist/download-git-userscript.user.js
@@ -3,7 +3,7 @@
 // @name:zh-CN 在线下载Github仓库文件夹
 // @description download github sub-folder via one click, copy the single file's source code easily
 // @description:zh-CN 无需克隆GitHub仓库, 一键在线下载 Github仓库子文件夹; 同时还能在源码详情页一键复制源码
-// @version 0.7.0
+// @version 0.7.1
 // @author Saiya
 // @supportURL https://github.com/oe/download-git-userscript/issues
 // @match https://github.com/*
